58166da57e9c01135ef09521f90ade66a72fdbfc
Vinícius <viniciusstorch62@gmail.com>
2025-09-26 11:11:18 -0300

    Script Modified

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public float jump;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jump);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
