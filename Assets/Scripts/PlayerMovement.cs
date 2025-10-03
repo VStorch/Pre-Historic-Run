c0999f7d890c2e8107ab7978eae03e8a46c8e2cf
Vinícius <viniciusstorch62@gmail.com>
2025-10-03 18:56:59 -0300

    Start of player jump development

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,23 @@ public class PlayerMoviment : MonoBehaviour
 {
     public float jump;
     private Rigidbody2D rb;
+    private Animator anim;
     private bool isGrounded;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        anim.SetBool("isGrounded", isGrounded);
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector2.up * jump);
+            anim.SetTrigger("Jump");
         }
     }
     private void OnCollisionEnter2D(Collision2D other)
