9fe2c5f0051a788a6ffdd3adc9383ef4a50bc2ac
Vinícius <viniciusstorch62@gmail.com>
2025-10-04 17:55:49 -0300

    Altered enemies

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,42 +3,61 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject[] spawnObject;
+    [Header("Spawn Objects")]
+    public GameObject[] groundEnemies;
+    public GameObject[] flyingEnemies;
+
+    [Header("Spawn Points")]
     public GameObject[] spawnPoints;
+
+    [Header("Spawn Time")]
     public float timer;
     public float timeBetweenSpawns;
 
+    [Header("Player")]
     public Animator playerAnimator;
     public float speedMultiplier;
     private float distance;
 
+    [Header("UI")]
     public Text distanceUI;
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        timeBetweenSpawns = Random.Range(1f, 3f);
     }
 
-    // Update is called once per frame
     void Update()
     {
         distanceUI.text = "Distance: " + distance.ToString("F2");
 
         speedMultiplier += Time.deltaTime * 0.1f;
-
         playerAnimator.speed = (float)(1 + speedMultiplier * 0.1);
-
         timer += Time.deltaTime;
-
         distance += Time.deltaTime * 0.8f;
 
         if (timer > timeBetweenSpawns)
         {
             timer = 0;
-            for (int i = 0; i < spawnObject.Length; i++)
+            timeBetweenSpawns = Random.Range(1f, 3f);
+
+            int randomPoint = Random.Range(0, spawnPoints.Length);
+
+            GameObject enemyToSpawn = null;
+
+            if (randomPoint == 0)
             {
-                Instantiate(spawnObject[i], spawnPoints[i].transform.position, Quaternion.identity);
+                int randomGround = Random.Range(0, groundEnemies.Length);
+                enemyToSpawn = groundEnemies[randomGround];
             }
+            else
+            {
+                int randomFlying = Random.Range(0, flyingEnemies.Length);
+                enemyToSpawn = flyingEnemies[randomFlying];
+            }
+
+                Instantiate(enemyToSpawn, spawnPoints[randomPoint].transform.position, Quaternion.identity);
+            
         }
     }
 }
